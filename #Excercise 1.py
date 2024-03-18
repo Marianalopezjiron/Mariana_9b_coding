296bfbbf6e54745eeb4e1b5f053fc5911057e72b
Mariana <mariana.lopez@student.goldenvalley.ed.cr>
2024-03-18 14:15:03 -0600

    Excercises

---
+++ #Excercise 1.py
@@ -0,0 +1,14 @@
+#Excercise 1
+name = input("Cual es su nombre?: ")
+print ("Hola", name)
+
+#Excercise 2
+name = input("Nombre?: ")
+age = int(input("Edad?: "))
+print("Hello", name, age)
+
+#Excercise 3
+num_hours = int(input("How many hours did you work?: "))
+cost_hour = (int(input("What is the cost per hour?: ")))
+payment = int(num_hours*cost_hour)
+print("The payment is", payment)
